78e57071d74089628bd8fbb40c6cf7a4d614d7fa
justmogen <justmogen@gmail.com>
2024-07-25 10:11:42 +0300

    Relay functionality to links

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -3,11 +3,22 @@ import graphql_jwt
 
 import links.schema
 import users.schema
+import links.schema_relay
 
-class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
+class Query(
+    links.schema.Query,
+    users.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType,
+):
     pass
 
-class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    links.schema.Mutation,
+    users.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    graphene.ObjectType,
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
